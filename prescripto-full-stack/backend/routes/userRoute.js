f4d68fe6bd4a7de382d534cbe518ef5e65bef0c6
Navneet Kumar <navneet.889035@gmail.com>
2025-08-10 22:13:01 +0530

    Update userRoute.js

--- prescripto-full-stack/backend/routes/userRoute.js
+++ prescripto-full-stack/backend/routes/userRoute.js
@@ -14,7 +14,6 @@ userRouter.get("/appointments", authUser, listAppointment)
 userRouter.post("/cancel-appointment", authUser, cancelAppointment)
 userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
 userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
-userRouter.post("/payment-stripe", authUser, paymentStripe)
-userRouter.post("/verifyStripe", authUser, verifyStripe)
+
 
 export default userRouter;
